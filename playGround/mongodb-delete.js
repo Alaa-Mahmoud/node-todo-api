32fa725c399d081e0ffa4d4bccd299efa87f7ed8
Alaa-Mahmoud <alaamahmoud.fci.scu@gmail.com>
2017-12-06 14:29:39 +0200

    add delete script

---
+++ playGround/mongodb-delete.js
@@ -0,0 +1,17 @@
+const { MongoClient, ObjectID } = require('mongodb');
+
+MongoClient.connect("mongodb://localhost:27017/TodoApp", (err, db) => {
+    if (err)
+        return console.log("Unable.connet to the server");
+
+    console.log("Connected to MongoDB server");
+
+    db.collection('Todos').deleteMany({ text: " create new todo" }).then((res) => {
+        console.log(res);
+    });
+
+
+
+    db.close();
+
+});
